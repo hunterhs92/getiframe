b12db572b6ea4e8f3d224757f0c921d74550fd77
LHS <hoangson.it.hut@gmail.com>
2024-10-21 18:40:25 +0700

    first commit

---
+++ pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 9.0.3 (trial), 000000, 2024-10-21T18:29:44.828647
+from .pyarmor_runtime import __pyarmor__
---
+++ run.bat
@@ -0,0 +1,37 @@
+@echo off
+title Get Iframe by @thic_autu
+chcp 65001 >nul
+if not exist .venv (
+    echo Đang tạo biến môi trường...
+    python -m venv .venv
+)
+
+echo Activating biến môi trường...
+call .venv\Scripts\activate
+
+if not exist .venv\Lib\site-packages\installed (
+    if exist requirements.txt (
+        echo Đang cài đặt wheel để cài đặt nhanh hơn
+        pip install wheel
+        echo Đang cài đặt các dependencies...
+        pip install -r requirements.txt
+        echo. > venv\Lib\site-packages\installed
+    ) else (
+        echo requirements.txt không tồn tại, bỏ qua việc cài đặt dependencies.
+    )
+) else (
+        echo Dependencies đã được cài đặt, bỏ qua việc cài đặt.
+    )
+
+if not exist .env (
+    echo Không tìm thấy file cấu hình .env. Thoát chương trình sau 30 giây...
+    timeout /t 30 /nobreak >nul
+    exit /b
+)
+
+:loop
+python main.py
+echo Đang khởi động lại chương trình sau 30 giây...
+timeout /t 30 /nobreak >nul
+goto :loop
+
